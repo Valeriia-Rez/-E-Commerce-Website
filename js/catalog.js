64e37c3673dbbe092fc70a188b002f17dca32d57
Valeriia-Rez <Valeria09122016@gmail.com>
2020-02-23 18:27:38 +0200

    Add open and close filters functionality

---
+++ js/catalog.js
@@ -0,0 +1,24 @@
+const openFiltersBtn = document.querySelector("[data-selector='filters_open']");
+const closeFiltersBtn = document.querySelector("[data-selector='filters_close']");
+const mobileFilters = document.querySelector("[data-selector='mobile_filters']");
+
+const openFiltersHandler = () => {
+    if (mobileFilters.classList.contains("d-none")) {
+        mobileFilters.classList.remove("d-none");
+        openFiltersBtn.classList.add("d-none");
+        closeFiltersBtn.classList.remove("d-none");
+    }
+
+}
+
+const closeFiltersHandler = () => {
+    if (!mobileFilters.classList.contains("d-none")) {
+        mobileFilters.classList.add("d-none");
+        openFiltersBtn.classList.remove("d-none");
+        closeFiltersBtn.classList.add("d-none");
+    }
+
+}
+
+openFiltersBtn.addEventListener("click", openFiltersHandler);
+closeFiltersBtn.addEventListener("click", closeFiltersHandler);
