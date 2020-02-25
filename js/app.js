ca62cd604c167787b4d0f4940c844bb331c0893e
Valeriia-Rez <Valeria09122016@gmail.com>
2020-02-25 17:06:02 +0200

    Implement mobile menu and Start implement Item Details Page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 const searchBtn = document.querySelector("[data-selector='search_icon']");
 const searchInput = document.querySelector("[data-selector='search_field']");
-console.log(searchBtn);
+const openMobileBtn = document.querySelector("[data-selector='mobile_open']");
+const closeMobileBtn = document.querySelector("[data-selector='mobile_close']");
+const mobileMenu = document.querySelector("[data-selector='mobile_menu']");
+
+
+
+
 
 const openSearch = () => {
-    console.log(searchInput);
-
     if (searchInput.classList.contains("d-none")) {
         searchInput.classList.remove("d-none");
     }
+}
+
+const openMobileHandler = () => {
+    if (mobileMenu.classList.contains("d-none")) {
+        mobileMenu.classList.remove("d-none");
+        openMobileBtn.classList.add("d-none");
+        closeMobileBtn.classList.remove("d-none");
+
+    }
+
+}
+
+const closeMobileHandler = () => {
+    if (!mobileMenu.classList.contains("d-none")) {
+        mobileMenu.classList.add("d-none");
+        openMobileBtn.classList.remove("d-none");
+        closeMobileBtn.classList.add("d-none");
+    }
+
 }
