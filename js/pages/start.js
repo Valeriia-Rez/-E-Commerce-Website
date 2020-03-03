1971c268c7791d48f1178e28d7a0b7e81eb630c2
Valeriia-Rez <Valeria09122016@gmail.com>
2020-03-03 16:49:30 +0200

    Implement best offer item rendering

--- js/pages/start.js
+++ js/pages/start.js
@@ -1,14 +1,39 @@
 const storage = new Storage();
 
-const addToBagBestOffer = () => {
-    const { items } = storage.getShoppingCart();
-}
+const addToBagBestOffer = e => {
+    const allProducts = window.catalog;
+    const { firstid, secondid } = e.target.dataset;
+    const bestOfferItemData = allProducts.filter(
+        item => item.id === firstid || item.id === secondid
+    );
+
+    bestOfferItemData.forEach(item => {
+        const finalProductPrice =
+            item.price !== item.discountedPrice && item.discountedPrice ?
+            item.discountedPrice :
+            item.price;
+        let addToShoppingBagProduct = {
+            ...item,
+            storageId: `storageID-${Math.floor(Math.random() * 10000 + 1)}`,
+            selectedSize: "",
+            selectedColor: "",
+            quantity: 1,
+            finalProductPrice,
+            discount: 15
+        };
+        storage.storeToShoppingCart(addToShoppingBagProduct);
+    });
+    renderHeaderComponent();
+    window.location.pathname = "/shopping-bag.html";
+};
 
 const renderBestOfferItems = () => {
     const allProducts = window.catalog;
     const itemIdOne = "8c061815-6a7d-4465-bb78-1bdc6c5adebf";
     const itemIdTwo = "5677f851-1c4a-4e9b-80e9-16d1e6265257";
-    const bestOfferItemData = allProducts.filter(item => item.id === itemIdOne || item.id === itemIdTwo);
+    const bestOfferItemData = allProducts.filter(
+        item => item.id === itemIdOne || item.id === itemIdTwo
+    );
     const bestOffer = document.querySelector("[data-selector='best_offer']");
     bestOffer.innerHTML = `
             <div class="product_items d-flex justify-around">
@@ -41,21 +66,17 @@ const renderBestOfferItems = () => {
                     <div class="price_after_discount f-size-26">
                         <span class="d-block ">£146.00</span>
                     </div>
-                    <div class="button_wrapper text-center d-none d-block-desktop-up" data-selector="addToBag">
-                        <a href="shopping-bag.html" class="btn f-size-20">Add to bag</a>
+                    <div class="button_wrapper text-center d-none d-block-desktop-up">
+                        <button data-firstid=${itemIdOne} data-secondid=${itemIdTwo} data-selector="addToBagButton" class="btn f-size-20">Add to bag</button>
                     </div>
                 </div>
             </div>
-            <div class="button_wrapper text-center d-none-desktop-up" data-selector="addToBag">
-                <a href="shopping-bag.html" class="btn  f-size-20">Add to bag</a>
+            <div class="button_wrapper text-center d-none-desktop-up">
+                <button data-firstid=${itemIdOne} data-secondid=${itemIdTwo} data-selector="addToBagButton" class="btn  f-size-20">Add to bag</button>
             </div>
           `;
-    const addToBag = document.querySelector("[data-selector='addToBag']");
+    const addToBag = document.querySelector("[data-selector='addToBagButton']");
     addToBag.addEventListener("click", addToBagBestOffer);
-}
-
-
-
-
+};
 
 window.addEventListener("DOMContentLoaded", renderBestOfferItems);
--- js/storage.js
+++ js/storage.js
@@ -1,13 +1,14 @@
 class Storage {
     storeToShoppingCart(product) {
         let shoppingCart;
-        if (localStorage.getItem('shoppingCart') === null) {
+        if (localStorage.getItem("shoppingCart") === null) {
             shoppingCart = {
                 items: [product],
                 totalCost: product.finalProductPrice,
-                itemsCount: 1
+                itemsCount: 1,
+                discount: null
             };
-            localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+            localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
         } else {
             this.updateShoppingCart(product);
         }
@@ -15,48 +16,81 @@ class Storage {
 
     getShoppingCart() {
         let shoppingCart;
-        if (localStorage.getItem('shoppingCart') === null) {
+        if (localStorage.getItem("shoppingCart") === null) {
             shoppingCart = {};
         } else {
-            shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+            shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
         }
         return shoppingCart;
     }
 
     updateShoppingCart(product) {
-        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-        const isItemExist = shoppingCart.items.some(item => item.storageId === product.storageId);
+        let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
+        const isItemExist = shoppingCart.items.some(
+            item => item.storageId === product.storageId
+        );
         if (isItemExist) {
-            const itemIndex = shoppingCart.items.findIndex(item => item.storageId === product.storageId);
+            const itemIndex = shoppingCart.items.findIndex(
+                item => item.storageId === product.storageId
+            );
             shoppingCart.items.splice(itemIndex, 1, {...product });
         } else {
             shoppingCart.items.push(product);
         }
-        const totalCost = shoppingCart.items.reduce((acc, cur) => acc + cur.quantity * cur.finalProductPrice, 0);
-        const itemsCount = shoppingCart.items.reduce((acc, cur) => acc + cur.quantity, 0)
+        const totalCost = shoppingCart.items.reduce(
+            (acc, cur) => acc + cur.quantity * cur.finalProductPrice,
+            0
+        );
+        const itemsCount = shoppingCart.items.reduce(
+            (acc, cur) => acc + cur.quantity,
+            0
+        );
         shoppingCart.itemsCount = itemsCount;
-        shoppingCart.totalCost = totalCost;
-        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+        shoppingCart.totalCost = product.discount ?
+            totalCost - product.discount :
+            totalCost;
+        shoppingCart.discount = this.hasDiscount(
+            shoppingCart.discount,
+            product.discount
+        );
+        localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+    }
+
+    hasDiscount(shoppingCartDiscount, productDiscount) {
+        if (!shoppingCartDiscount && !productDiscount) {
+            return null;
+        }
+        if (!shoppingCartDiscount && productDiscount) {
+            return productDiscount;
+        }
+        if (shoppingCartDiscount && productDiscount) {
+            return shoppingCartDiscount + productDiscount;
+        }
+        if (!productDiscount && shoppingCartDiscount) {
+            return shoppingCartDiscount;
+        }
     }
 
     deleteItemFromShoppingCart(id) {
         let updatedShoppingCartItems, updatedShoppingCart;
-        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+        let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
         shoppingCart.items.forEach(function(item, index) {
             if (id === item.storageId) {
-                updatedShoppingCartItems = shoppingCart.items.filter(item => item.storageId !== id);
+                updatedShoppingCartItems = shoppingCart.items.filter(
+                    item => item.storageId !== id
+                );
                 updatedShoppingCart = {
                     items: updatedShoppingCartItems,
                     itemsCount: shoppingCart.itemsCount - item.quantity,
                     totalCost: shoppingCart.totalCost - item.quantity * item.finalProductPrice
-                }
+                };
             }
         });
 
-        localStorage.setItem('shoppingCart', JSON.stringify(updatedShoppingCart));
+        localStorage.setItem("shoppingCart", JSON.stringify(updatedShoppingCart));
     }
 
     clearShoppingCart() {
-        localStorage.removeItem('shoppingCart');
+        localStorage.removeItem("shoppingCart");
     }
 }
